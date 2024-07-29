fbba4e1991085c04a64ee10d6108f5bc33291cdc
Samantha Mac <70736045+samanthavmac@users.noreply.github.com>
2024-07-29 19:31:29 -0400

    index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,14 @@
-import GoogleSignInButton from '../components/GoogleSignInButton';
+import styles from '../styles/onboarding.module.scss';
 
 export default function Home() {
   return (
-    <div>
-      <h1>Hello! Welcome.</h1>
-      <GoogleSignInButton/>
+    <div className={styles['main-center-container']}>
+      <h1>Make a difference with Dono</h1>
+      <p>Gamify community involvement with Dono. Connect with volunteer opportunities in your local community. Collect unique badges when you volunteer at a new non-profit, and gain tokens to the Dono store.</p>
+      <div className="btn-group">
+        <button>Donor (Volunteer)</button>
+        <button>Donee (Registered non-profit)</button>
+      </div>
     </div>
   );
 }
