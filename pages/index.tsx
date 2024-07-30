e9b69aeb09de9530f471b133d40cf1a3eae9e13d
Samantha Mac <70736045+samanthavmac@users.noreply.github.com>
2024-07-29 22:43:58 -0400

    donor onboarding frontend

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,13 @@
-import styles from '../styles/onboarding.module.scss';
+import styles from '@/styles/onboarding.module.scss';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <div className={styles['main-center-container']}>
       <h1>Make a difference with Dono</h1>
-      <p>Gamify community involvement with Dono. Connect with volunteer opportunities in your local community. Collect unique badges when you volunteer at a new non-profit, and gain tokens to the Dono store.</p>
+      <p className={styles['instruction-subtitle']}>Connect with volunteer opportunities in your local community. Collect unique badges when you volunteer at a new non-profit, and gain tokens to the Dono store.</p>
       <div className="btn-group">
-        <button>Donor (Volunteer)</button>
+        <Link href="/onboarding/donor-account-login"><button>Donor (Volunteer)</button></Link>
         <button>Donee (Registered non-profit)</button>
       </div>
     </div>
